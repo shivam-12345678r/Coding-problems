a470963a028736fbbc86d01e2ef7fe14ace8c587
shivam-12345678r <138965330+shivam-12345678r@users.noreply.github.com>
2025-04-01 10:58:17 +0530

    Time: 4 ms (8.96%) | Memory: 12.3 MB (80.52%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def rob(self,nums):
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+
+        def rob_linear(nums):
+            n = len(nums)
+            if n == 0:
+                return 0
+            if n == 1:
+                return nums[0]
+
+            dp = [0] * n
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+
+            for i in range(2, n):
+                dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+            return dp[n - 1]
+
+        return max(rob_linear(nums[:-1]), rob_linear(nums[1:]))
+
+      
+        
