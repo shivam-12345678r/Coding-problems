006b5149eaeee0eb9bf073b702c02278adc06ab1
shivam-12345678r <138965330+shivam-12345678r@users.noreply.github.com>
2025-04-06 10:45:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (52.02%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,13 +1,8 @@
-class Solution(object):
-    def hammingWeight(self, n):
+class Solution:
+    def hammingWeight(self, n: int) -> int:
         cnt = 0
         while n:
-            if n & 1 == 1:
-                cnt += 1
-            n = n//2
+            cnt += (n & 1)
+            n >>= 1
         return cnt
-        """
-        :type n: int
-        :rtype: int
-        """
         
