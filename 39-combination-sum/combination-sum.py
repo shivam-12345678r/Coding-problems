ab924a0448e5321267a95d59ef3185e958587b2f
shivam-12345678r <138965330+shivam-12345678r@users.noreply.github.com>
2025-04-19 09:22:01 +0530

    Time: 11 ms (59.01%) | Memory: 18.1 MB (20.42%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,11 +1,19 @@
+from typing import List
+from functools import lru_cache
+
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        dp = [[] for _ in range(target + 1)]
-        dp[0] = [[]]
-        for can in candidates:
-            for i in range(can,target + 1):
-                for comb in dp[i - can]:
-                    dp[i].append(comb + [can])
-        return dp[target]
+        res = []
 
-        
+        def dfs(start, target, path):
+            if target == 0:
+                res.append(path[:])
+                return
+            if target < 0:
+                return
+
+            for i in range(start, len(candidates)):
+                dfs(i, target - candidates[i], path + [candidates[i]])
+
+        dfs(0, target, [])
+        return res
