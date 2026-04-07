1377bb68a2e1966917ce83790379b3ed72d6bb2f
shivam-12345678r <138965330+shivam-12345678r@users.noreply.github.com>
2026-04-07 09:15:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (98.00%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def search(self, nums, target):
+        low , high = 0 , len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
